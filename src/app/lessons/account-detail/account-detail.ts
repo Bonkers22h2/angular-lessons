b10b6f86266d5453f4c3afe6fd29a7577dfef5c4
Bonkers <bonkcat78@gmail.com>
2026-08-20 13:20:48 +0800

    FEAT: Withdraw

--- src/app/lessons/account-detail/account-detail.ts
+++ src/app/lessons/account-detail/account-detail.ts
@@ -2,27 +2,48 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AccountService } from '../account';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";
 @Component({
   selector: 'app-account-detail',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './account-detail.html',
   styleUrl: './account-detail.css',
 })
 export class AccountDetail implements OnInit {
   account: any = null;
+  amount: number = 0;
+  accountNumber: string = '';
 
 
   constructor(private route: ActivatedRoute, private accountService: AccountService, private cdr: ChangeDetectorRef) {
   }
 
-  ngOnInit(){
-    const accountNumber = this.route.snapshot.paramMap.get('accountNumber') ?? '';
+  ngOnInit() {
+    this.accountNumber = this.route.snapshot.paramMap.get('accountNumber') ?? '';
 
-    this.accountService.getAccount(accountNumber).subscribe(data => {
+    this.accountService.getAccount(this.accountNumber).subscribe(data => {
       this.account = data;
       this.cdr.detectChanges();
     })
+  }
 
+  loadAccount() {
+    const accountNumber = this.route.snapshot.paramMap.get('accountNumber') ?? '';
+    this.accountService.getAccount(accountNumber).subscribe(data => {
+      this.account = data;
+      this.cdr.detectChanges();
+    });
+  }
+  withdraw() {
+    this.accountService.withdraw(this.accountNumber, this.amount).subscribe({
+      next: () => {
+        this.amount = 0;
+        this.loadAccount();
+      },
+      error: (err) => {
+        console.error('Withdraw failed:', err.error?.message);
+      }
+    });
   }
 
 
